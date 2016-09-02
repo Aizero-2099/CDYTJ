be88b1afbccf5526a4c4375243035cc5b2b27a53
KingMarshal <1420598691@qq.com>
2016-09-02 21:32:47 +0800

    新建了类

---
+++ app/src/main/java/com/cdytj/cdytj/activity/AdActivity.java
@@ -0,0 +1,15 @@
+package com.cdytj.cdytj.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cdytj.cdytj.R;
+
+public class AdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad);
+    }
+}
--- app/src/main/java/com/cdytj/cdytj/activity/SplashActivity.java
+++ app/src/main/java/com/cdytj/cdytj/activity/SplashActivity.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 
 import com.cdytj.cdytj.R;
 
+import cn.bmob.v3.Bmob;
+
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Bmob.initialize(this,"4290863f4edb1aa0eafb6f0438ca58cd");//用Bmob后台新建应用时提供的APP_ID来在应用中初始化Bmob的服务
+
     }
 }
